5cf8023899c03e0b321cb036f3291a66a4218d15
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-09-17 22:49:14 +0200

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo
+echo instalador miip miip2
+echo Se instalaran as utilidades en /sbin/ para poder ser ejecutardo como comandos de Linux
+echo
+chmod 777 miip
+chmod 777 miip2
+cp miip /sbin/
+cp miip2 /sbin/
+echo
+echo "Instalacion finalizada."
+echo
